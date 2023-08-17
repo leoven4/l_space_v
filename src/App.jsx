e0c5af37cbaab6d3dc287287c7305cc918d3c64c
leoven4 <leonardo.ventura424@gmail.com>
2023-08-17 11:23:40 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from "./routes/navigation/navigation.jsx"
 import { Routes, Route } from 'react-router-dom'  // must be nested in a BrowserRouter component
 import Gallery from "./routes/gallery/Gallery.jsx";
 
+
 const App = () => {
   return (
     <Routes>
